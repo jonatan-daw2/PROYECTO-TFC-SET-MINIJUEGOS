0d1dd7de5eec8bd3bda3aafe60fca527811bce99
jonatan-daw2 <rosariomatosj@gmail.com>
2023-06-08 12:22:30 +0200

    correciones

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title>Inicio</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Agregamos los estilos de Bootstrap -->
-    <link rel="stylesheet" href="./proyecto/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="../proyecto/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="../proyecto/css/inicio.css">
     <script src="../proyecto/js/app2.js"></script>
 </head>
